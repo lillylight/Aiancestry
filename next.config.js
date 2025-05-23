22b6fff2ae508d6b112229ce0abe7cbf3b8a776d
Your <you@example.com>
2025-05-23 09:30:41 +0200

    fix: Update Next.js configuration for Vercel
    - Remove deprecated @zeit/next-css and @zeit/next-sass packages
    - Use Next.js built-in CSS and Sass support
    - Update configuration to use TypeScript properly

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-
-module.exports = withCSS(withSass({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(css|scss)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            implementation: require('sass'),
-          },
-        },
-      ]
-    });
     return config;
   }
-}));
+}
+
+module.exports = nextConfig;
