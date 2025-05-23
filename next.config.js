1ee9f09fcf4f92014855320b1f656732588247bb
Your <you@example.com>
2025-05-23 09:25:47 +0200

    fix: Update build configuration for Vercel
    - Add Node.js version constraint (>=18.0.0)
    - Add Sass and CSS support
    - Update webpack configuration
    - Remove turbopack from dev script

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-module.exports = {
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withCSS(withSass({
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(css|scss)$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass'),
+          },
+        },
+      ]
     });
     return config;
-  },
-  experimental: {
-    disableLightningcss: true
   }
-};
+}));
